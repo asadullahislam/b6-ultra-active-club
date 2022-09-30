d9e456e75d3f4b17005f3a741b303ae3ef452d0f
asadullahislam <asadullah0072002@gmail.com>
2022-09-30 16:17:04 +0600

    break time added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,28 +1,34 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 // import Routine from '../Routine/Routine';
-// import asad from '../../../public/images/asad.JPG'
-import './Cart.css'
+// import asad from  '../../../public/images/asad.JPG'
 
-const Cart = ({cart}) => {
-   
 
- let total = 0;
- for(const routine of cart){
-    total = total +routine.duration;
- }
+import './Cart.css';
 
 
 
- 
 
 
 
- 
+const Cart = ({ cart }) => {
+
+    const [breaktime, setBreaktime] = useState(0);
+
+
+    let total = 0;
+    for (const routine of cart) {
+        total = total + routine.duration;
+    }
+
+    const addToBreakTime = (breakTime) => {
+        setBreaktime(breakTime)
+
+    }
+
     return (
-        <div className='cart' >
+        <div className='cart'>
             <div className='name-container'>
-                {/* <img src={asad} alt="" /> */}
-                <p>hello</p>
+                <img src="" alt="" />
                 <div>
                     <p>Md.Asadullah</p>
                     <p><small>Dhaka,Bangladesh</small></p>
@@ -31,39 +37,45 @@ const Cart = ({cart}) => {
             <div className='body-structure'>
                 <div>
                     <p><span className='weight-height-age'>75</span>kg <br />Weight</p>
-                   
+
                 </div>
                 <div>
                     <p><span className='weight-height-age'>6.25</span>ft
-                    <br />Height</p>
-                    
+                        <br />Height</p>
+
                 </div>
                 <div>
                     <p><span className='weight-height-age'>25</span>years <br />Age</p>
-                   
+
                 </div>
             </div>
 
             <h3>Add a Break</h3>
 
-            <div className='total-break'>
-                <p className='break-time'><span  >10</span>m</p>
-                <p className='break-time'><span>20</span>m</p>
-                <p className='break-time'><span>30</span>m</p>
-                <p className='break-time'><span>40</span>m</p>
-                <p className='break-time'><span>50</span>m</p>
+            <div classNameclassName='total-break'>
+                <button onClick={()=>addToBreakTime(10)} className='break-time'>10m</button>
+                <button onClick={()=>addToBreakTime(20)} className='break-time'>20m</button>
+                <button onClick={() => addToBreakTime(30)} className='break-time'>30m</button>
+                <button onClick={() => addToBreakTime(40)} className='break-time'>40m</button>
+                <button onClick={() => addToBreakTime(50)} className='break-time'>50m</button>
+
             </div>
+
             <h3> Duration Details</h3>
+
             <div>
-                <h5 className='time-spent'>Time Spent:{total}</h5>
+                <h5 className='time-spent'>Time Spent: {total} Minute</h5>
             </div>
             <div>
-                <h5 className='time-spent'>Break Time:</h5>
+                <h5 className='time-spent'>Break Time:{breaktime}  Minute</h5>
             </div>
-            <button className='btn'>
+            <button>
                 <p className='btn-text'>Activity Completed</p>
             </button>
-        </div>
+
+
+
+        </div >
     );
 };
 
--- src/Components/Routine/Routine.js
+++ src/Components/Routine/Routine.js
@@ -2,43 +2,49 @@ import React, { useEffect, useState } from 'react';
 import Activities from '../Activities/Activities';
 import Cart from '../Cart/Cart';
 
-import './Routine.css'
+import './Routine.css';
 
 const Routine = () => {
-const[routines,setRoutines]=useState([]);
-const[cart,setCart]=useState([]);
+    const [routines, setRoutines] = useState([]);
+    const [cart, setCart] = useState([]);
 
-useEffect(()=>{
-    fetch('routines.json')
-    .then(res=>res.json()) 
-    .then(data=>setRoutines(data))
+    useEffect(() => {
+        fetch('routines.json')
+            .then(res => res.json())
+            .then(data => setRoutines(data))
 
-},[])
+    }, [])
 
-const handleAddToCart=(routine)=>{
+    const handleAddToCart = (routine) => {
 
-    const newCart =[...cart,routine]
-    setCart(newCart);
+        const newCart = [...cart, routine]
+        setCart(newCart);
 
-}
+    }
+
+
 
     return (
         <div className='container'>
             <h4 >Today My Routine</h4>
             <div className='routine-container'>
                 <div className='routine-div'>
-                  
+
                     {
-                     routines.map(routine=><Activities
-                         key ={routine.id}
-                         routine ={routine}
-                         handleAddToCart={handleAddToCart}
-                    
-                     ></Activities>)
-                     }
+                        routines.map(routine => <Activities
+                            key={routine.id}
+                            routine={routine}
+                            handleAddToCart={handleAddToCart}
+
+
+                        ></Activities>)
+                    }
                 </div>
                 <div className='break-container'>
-                <Cart cart={cart}></Cart>
+                    <Cart cart={cart}
+
+
+                    ></Cart>
 
                 </div>
             </div>
